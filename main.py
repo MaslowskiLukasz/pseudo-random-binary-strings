39dfe4aa888c9abe1dfc250b31a44a167a502515
Maslowski Lukasz <l.maslowski2012@gmail.com>
2021-11-12 18:36:54 +0100

    add long series test and poker test

--- main.py
+++ main.py
@@ -11,5 +11,4 @@ r = 20000
 result = generator.generate_BBS(n, a, r)
 files.save_to_file(result_file_name, result)
 
-print(test.single_bit_test(result))
-print(test.series_test(result))
+print(f"all tests result: {test.run_all_tests(result)}")
--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 import pandas as pd
 from pandas import Series
 
+def run_all_tests(data):
+  single_bit_result = single_bit_test(data)
+  series_result = series_test(data)
+  long_series_result = long_series_test(data)
+  poker_result = poker_test(data)
+
+  print(f"single bit: {single_bit_result}")
+  print(f"series: {series_result}")
+  print(f"long series: {long_series_result}")
+  print(f"poker: {poker_result}")
+
+  return single_bit_result and series_result and long_series_result and poker_result
+
 def single_bit_test(data):
   counter = 0
   for i in range(0, len(data)):
@@ -9,6 +22,32 @@ def single_bit_test(data):
 
   print(f"number of ones = {counter}")
   return 9725 < counter < 10275
+
+def poker_test(data):
+  counters = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  for i in range(0, len(data) - 3, 4):
+    number = int(data[i : i+4], 2)
+    counters[number] += 1
+
+  sum = 0
+  for x in counters:
+    sum += x * x
+  test_value = (16/5000) * sum - 5000
+  return 2.16 < test_value < 46.17
+
+def long_series_test(data):
+  result = False
+  ones_series_counter = 0
+  zeros_series_counter = 0
+  convert_data = pd.Series(data)
+  ones_series_counter = convert_data.str.count("1{26,}")
+  zeros_series_counter = convert_data.str.count("0{26,}")
+
+  if ones_series_counter[0] == 0 & zeros_series_counter[0] == 0:
+    result = True
+  else:
+    result = False
+  return result
 
 def series_test(data):
   ones_series_counters = [0, 0, 0, 0, 0, 0] #series: 1, 11, 111, 1111, 11111, six or more 1
@@ -20,7 +59,10 @@ def series_test(data):
     if i == len(data) - 1 and data[i] == "1":
       ones_series_counters[current_ones_series_length] += 1
     elif data[i] == "1" and data[i+1] == "0":
-      ones_series_counters[current_ones_series_length] += 1
+      if current_ones_series_length > 5:
+        ones_series_counters[5] += 1
+      else:
+        ones_series_counters[current_ones_series_length] += 1
       current_ones_series_length = 0
     elif data[i] == "1" and data[i+1] == "1":
       current_ones_series_length += 1
@@ -28,7 +70,10 @@ def series_test(data):
     if i == len(data) - 1 and data[i] == "0":
       zeros_series_counters[current_zeros_series_length] += 1
     elif data[i] == "0" and data[i+1] == "1":
-      zeros_series_counters[current_zeros_series_length] += 1
+      if current_zeros_series_length > 5:
+        zeros_series_counters[5] += 1
+      else:
+        zeros_series_counters[current_zeros_series_length] += 1
       current_zeros_series_length = 0
     elif data[i] == "0" and data[i+1] == "0":
       current_zeros_series_length += 1 
