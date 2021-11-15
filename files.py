867e8569b78ac04deb611184e9ec8c84724f1d71
MaslowskiLukasz <l.maslowski2012@gmail.com>
2021-11-16 00:29:07 +0100

    add read data from file, check length of input file

--- files.py
+++ files.py
@@ -1,3 +1,15 @@
 def save_to_file(file_name, data):
   with open(file_name, "w") as file:
     file.write(data)
+
+def read_from_file(file_name):
+  text = ""
+  with open(file_name, "r") as file:
+    text = file.read()
+  return text
+
+def validate_input(data):
+  if len(data) != 20_000:
+    print("Wrong input size")
+    return False
+  return True
